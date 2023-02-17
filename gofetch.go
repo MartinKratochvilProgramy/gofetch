4db2785bc322fe091213c95157bad26cb9624ccc
MartinKratochvilProgramy <martvil96@gmail.com>
2023-02-17 13:39:51 +0100

    changed ticker info

--- gofetch.go
+++ gofetch.go
@@ -10,7 +10,12 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-func fetchPrevClose(ticker string) float64 {
+type TickerInfo struct {
+	ticker string
+	price  float64
+}
+
+func FetchPrevClose(ticker string) *TickerInfo {
 	resp, err := http.Get("https://query1.finance.yahoo.com/v8/finance/chart/" + ticker)
 	if err != nil {
 		log.Fatalln(err)
@@ -26,12 +31,10 @@ func fetchPrevClose(ticker string) float64 {
 	res_2 := strings.Replace(res_1, "[", "", -1)
 	result := gjson.Get(res_2, "chart.result.meta.chartPreviousClose").Float()
 
-	return result
-}
-
-type TickerInfo struct {
-	ticker string
-	price  float64
+	return &TickerInfo{
+		ticker: ticker,
+		price:  result,
+	}
 }
 
 func FetchAsync(tickers []string) []TickerInfo {
@@ -42,8 +45,8 @@ func FetchAsync(tickers []string) []TickerInfo {
 
 	for i, ticker := range tickers {
 		go func(i int, ticker string) {
-			price := fetchPrevClose(ticker)
-			res = append(res, TickerInfo{ticker: ticker, price: price})
+			info := FetchPrevClose(ticker)
+			res = append(res, *info)
 			wg.Done()
 		}(i, ticker)
 	}
